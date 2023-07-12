5039904175e794bcf18d0071245c7ab8db0eb649
Hyeyeon Kim <hyeyeonismm@gmail.com>
2023-07-12 20:55:08 +0900

    Update [PGS] convertStr.cpp

--- hyeyeonismm/[PGS] convertStr.cpp
+++ hyeyeonismm/[PGS] convertStr.cpp
@@ -5,8 +5,8 @@ int main() {
 	string str;
 	cin >> str;
 
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] <= 90) {
+	for (int i = 0; i < str.length(); i++) { 
+		if (str[i] <= 90) { //ASCII 코드 사용하여 변환
 			str[i] += 32;
 		}
 		else
