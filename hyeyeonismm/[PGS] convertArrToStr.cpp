5538b1b675cf4f6b87eb86ffcebabfada6d04e71
Hyeyeon Kim <hyeyeonismm@gmail.com>
2023-07-13 15:00:45 +0900

    Create [PGS] convertArrToStr.cpp

---
+++ hyeyeonismm/[PGS] convertArrToStr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> arr) {
+    string answer = "";
+    for (string s: arr) {
+        answer += s;
+    }
+    return answer;
+}
